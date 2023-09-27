194ffa7240fde48f243a94dfb8cf63b47e2620be
MrKarimov01 <tungiborilar094@gmail.com>
2023-09-28 03:08:34 +0500

    Console ban qo'shildi

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,19 @@ import Home from './pages/home/Home';
 import About from './pages/about/About';
 import Portfolio from './pages/portfolio/Portfolio';
 import Contact from './pages/contact/Contact';
+import DisableDevtool from 'disable-devtool';
+import { init } from 'console-ban'
+
+DisableDevtool();
+const div = document.createElement('div')
+div.innerHTML = 'Stop'
+
+init({
+  // Rewrite the <body></body> as a string
+  write: '<h1> Stop </h1>',
+  // or rewrite a element
+  write: div
+})
 
 function App() {
   return (
